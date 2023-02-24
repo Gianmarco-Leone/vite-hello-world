73ea87e78621561f1356a141e5c482640a65dfc0
Gianmarco-Leone <gianmarcoleone96@gmail.com>
2023-02-24 14:47:19 +0100

    add bonus: add other component with jumbotron

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,21 @@
 <script>
+// Importo componente AppTitle.vue
 import AppTitle from "./components/AppTitle.vue";
 
+// Importo componente AppHero.vue
+import AppHero from "./components/AppHero.vue";
+
+// Esporto componenti
 export default {
   components: {
     AppTitle,
+    AppHero,
   },
 };
 </script>
 
+<!-- Inserisco componenti nel template -->
 <template>
   <AppTitle />
+  <AppHero />
 </template>
---
+++ src/components/AppHero.vue
@@ -0,0 +1,21 @@
+<script>
+//  Qui va il codice javascript
+</script>
+
+<template>
+  <div id="jumbotron">
+    <h2>Il mio Jumbotron</h2>
+  </div>
+</template>
+
+<style scoped>
+/* Qui va il codice CSS */
+#jumbotron {
+  height: calc(100vh - 100px);
+  background-color: #666;
+  color: #ddd;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
