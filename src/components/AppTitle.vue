830a2c27a467e492464bbb42b9227aac5ef8c705
Gianmarco-Leone <gianmarcoleone96@gmail.com>
2023-02-24 14:32:30 +0100

    add component with title

---
+++ src/components/AppTitle.vue
@@ -0,0 +1,21 @@
+<script>
+// Qui va il codice javascript
+</script>
+
+<template>
+  <header>
+    <h1>La mia prima app con Vite!</h1>
+  </header>
+</template>
+
+<style scoped>
+/* Qui va il codice CSS */
+header {
+  height: 100px;
+  background-color: #222;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: limegreen;
+}
+</style>
